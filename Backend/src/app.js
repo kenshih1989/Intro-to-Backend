d4a29462a1baeae3a18eec83498112fe2dd010c7
Gan Leng Keong <kenshih_keong@hotmail.com>
2026-05-07 22:59:30 +0800

    Complete CRUD

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,9 +6,11 @@ app.use(express.json()); // Middleware to parse JSON request bodies
 
 // routes import
 import userRouter from "./routes/user-routes.js";
+import postRouter from './routes/post-route.js';
 
 // routes declaration
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
 
 // example route: http://localhost:4000/api/v1/users/register
 
